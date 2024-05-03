3c623de604a4c51b3dc4e5d6c8df1762124e341a
emasjedi <emasjedi@umich.edu>
2024-05-03 09:59:36 -0700

    Update for when both expected and audience are arrays

--- lib.js
+++ lib.js
@@ -125,16 +125,21 @@ function verifyAudience(expected, aud) {
   if (!expected) {
     throw new Error('expected audience is required');
   }
+
   if (!Array.isArray(aud)) {
     if (Array.isArray(expected) && !expected.includes(aud)) {
       throw new Error(`audience claim ${aud} does not match one of the expected audiences: ${expected.join(', ')}`);
     }
-  
+
     if (!Array.isArray(expected) && aud !== expected) {
       throw new Error(`audience claim ${aud} does not match expected audience: ${expected}`);
     }
   } else {
-    if (!aud.includes(expected)) {
+    if (Array.isArray(expected) && !(aud.some(val => expected.includes(val)))) {
+      throw new Error(`audience claims ${aud.join(', ')} do not match one of the expected audiences: ${expected}`);
+    }
+
+    if (!Array.isArray(expected) && !aud.includes(expected)) {
       throw new Error(`audience claims ${aud.join(', ')} do not include expected audience: ${expected}`);
     }
   }
